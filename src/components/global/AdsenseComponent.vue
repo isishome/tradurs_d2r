c43b4ec4431cac69b797b5f0d49b888f1fcddd78
isishome <isishome@hanmail.net>
2025-02-16 14:59:02 +0900

    deploy

--- src/components/global/AdsenseComponent.vue
+++ src/components/global/AdsenseComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onBeforeMount, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 interface IProps {
   dataAdClient?: string
